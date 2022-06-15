4d4c766a109b9ba3e379290c074518d17d2b024c
mikaildokgoz <mikaildokgoz@gmail.com>
2022-06-15 13:43:25 +0300

    instagram link added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
           <a href="https://www.linkedin.com/in/mikail-dokg%C3%B6z/" target="blank">
             <i class="fab fa-linkedin fa-2x"></i>
           </a>
-          <a href="#">
+          <a href="https://www.instagram.com/">
             <i class="fab fa-instagram fa-2x"></i>
           </a>
           <a href="https://github.com/Mikaildokgoz" target="blank">
